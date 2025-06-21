1f0e113b48541c05327767cbf022928e112756e5
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-21 18:51:47 -0400

    completed problem 04

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,9 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let boolCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "boolean") boolCount++;
+  }
+  return boolCount;
 }
 
 // Do not edit this line;
