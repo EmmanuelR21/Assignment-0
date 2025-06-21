1f94f5aba7bacca012731d4069ace32ac8dcefae
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-21 18:54:49 -0400

    completed 05

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,10 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  let boolOrStringCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "boolean" || typeof arr[i] === "string")
+      boolOrStringCount++;
+  }
+  return boolOrStringCount;
 }
 
 // Do not edit this line;
