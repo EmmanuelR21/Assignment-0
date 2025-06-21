dbeac8ee9624abbe2f4a0de7a04ed48dd8dfb566
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-21 18:07:25 -0400

    completed problem 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,15 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  const letters = title.split("");
+  let isUpperCase = true;
+  for (let i = 0; i < letters.length; i++) {
+    if (isUpperCase) {
+      letters[i] = letters[i].toUpperCase();
+      isUpperCase = false;
+    } else if (letters[i] == " ") {
+      isUpperCase = true;
+    }
+  }
+  return letters.join("");
 }
 
 // Do not edit this line;
