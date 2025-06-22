6cb3621e699a6b4569fdb0d59094897dfbb335df
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-21 21:34:56 -0400

    completed problem 12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,14 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  const arr = [];
+  for (let i = start; i <= end; i++) {
+    let string = "";
+    if (i % 3 == 0) string += "Fizz";
+    if (i % 5 == 0) string += "Buzz";
+    if (string.length > 0) {
+      arr.push(string);
+    } else arr.push(i);
+  }
+  return arr;
 }
 
 // Do not edit this line;
