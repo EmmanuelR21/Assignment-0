d9f102641c53bb971d3c62232524edba1f70a596
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-23 10:27:49 -0400

    completed problem 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,15 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  const hash = {};
+
+  for (let i = 0; i < word.length; i++) {
+    if (!hash[word[i]]) {
+      hash[word[i]] = 1;
+    } else {
+      hash[word[i]]++;
+    }
+  }
+
+  return hash;
 }
 
 // Do not edit this line;
