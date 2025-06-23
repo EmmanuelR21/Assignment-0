4990c1adf28c4a78df59878a3c9e5ed514e36135
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-23 10:50:56 -0400

    completed problem 16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,11 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  const arr = [...args];
+  let product = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 }
 
 // Do not edit this line;
